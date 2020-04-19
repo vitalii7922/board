606d0b67d8bd24758b9caaf7d7d83a19fdce776e
vitalii7922 <nefedovvi792@gmail.com>
2020-04-19 23:18:04 +0300

    commit

--- board-web/src/main/java/com/tsystems/javaschool/controller/ScheduleController.java
+++ board-web/src/main/java/com/tsystems/javaschool/controller/ScheduleController.java
@@ -1,14 +1,35 @@
 package com.tsystems.javaschool.controller;
 
+import com.tsystems.javaschool.dto.ScheduleDto;
 
-import com.tsystems.javaschool.service.ScheduleService;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-import javax.enterprise.inject.Model;
-import javax.inject.Inject;
-
-@Model
 public class ScheduleController {
 
-    @Inject
-    private ScheduleService scheduleService;
+    private ScheduleDto scheduleDto;
+
+    public static void main(String[] args) {
+
+        try {
+            URL url = new URL("http://localhost:8181/schedules/421");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+            conn.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
